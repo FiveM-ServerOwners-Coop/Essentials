fe51ed080255b618998b0c34483eb5ae1c38c6c9
Danish Khan <61810778+PLOKMJNB@users.noreply.github.com>
2021-04-25 08:05:22 -0400

    remove map limits

--- client/coreThread.lua
+++ client/coreThread.lua
@@ -40,4 +40,20 @@ CreateThread(function()
             Wait(0)
         end
     end
+
+    ---
+    --- Remove Map Limits
+    ---
+    Wait(0)
+        ExpandWorldLimits(
+            -9000.0,
+            -11000.0,
+            30.0
+        )  
+        ExpandWorldLimits(
+            10000.0,
+            12000.0,
+            30.0
+        ) 		
+    end
 end)
